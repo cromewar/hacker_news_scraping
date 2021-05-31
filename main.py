49d6a9d55f2137e2bdab668f3f2cbd7beac25367
cromewar <vasiliy@cromewar.com>
2021-05-31 11:14:36 -0500

    basic beautiful soup manipulation

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
+response = requests.get('https://news.ycombinator.com/news')
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+links = soup.select('.storylink')
+votes = soup.select('.score')
+
+def create_hn(links, votes):
+    hn = []
+    for idx, item in enumerate(links):
+        title = links[idx].getText()
+        href = links[idx].get('href', None)
+        hn.append({'title': title, 'link': href})
+    return hn
+
+print(create_hn(links, votes))
